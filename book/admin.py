d87f7fa2fbfdc48775ca85bb86da6e9ecb29ced1
oyarso <sam.copae@gmail.com>
2024-10-04 13:43:37 -0300

    nuevo commit

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
-
-# Register your models here.
+from django.contrib import admin 
+# Register your models here. 
+from .models import BoardsModel 
+admin.site.site_header = 'Curso Django' 
+admin.site.index_title = 'Panel de control Proyecto Django' 
+admin.site.site_title = 'Administrador Django' 
+class BoardsAdmin(admin.ModelAdmin): 
+    readonly_fields = ('creado', 'modificado') 
+    list_display = ('clasificacion', 'titulo', 'valor') 
+    search_fields = ('titulo', 'descripcion') 
+    ordering = ('valor',) 
+    list_filter = ('creado','valor') 
+    def clasificacion(self, obj): 
+        return "Alto" if obj.valor >= 5 else "Bajo" 
+admin.site.register(BoardsModel, BoardsAdmin)  
--- book/models.py
+++ book/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.db import models 
+# Create your models here. 
+class BoardsModel(models.Model): 
+# Campos del modelo 
+    titulo = models.CharField(max_length = 200) 
+    descripcion = models.TextField()     
+    valor = models.FloatField() 
+    creado = models.DateTimeField(auto_now_add=True) 
+    modificado = models.DateTimeField(auto_now = True) 
 
-# Create your models here.
+class Meta: 
+    verbose_name = "tablero" 
+    verbose_name_plural = "tableros" 
+    ordering = ["-creado"]         
+    permissions = ( 
+        ("es_miembro_1", "Es miembro con prioridad 1"), 
+    )   
+def __str__(self): 
+    return self.descripcion 
+
